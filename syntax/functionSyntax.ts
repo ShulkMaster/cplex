0f8683634f866f8585ed3ad13fbef5f5fc405165
Yury Rivera <yrivera@applaudostudios.com>
2022-11-28 03:49:52 -0600

    terminando tabla de parseo

--- syntax/functionSyntax.ts
+++ syntax/functionSyntax.ts
@@ -3,14 +3,20 @@ import {mapSet, ProductionSet} from './Set.js';
 
 export type FuncDeclaration = 'FuncDeclaration';
 export type FuncDeclarationPrime = 'FuncDeclarationPrime';
-export type CompoundStatement = 'compoundStatement'
-export type Bi = 'bI'
+export type compoundStatement = 'compoundStatement';
+export type declarationPrime = 'declarationPrime';
+export type FD1 = 'fD1';
+export type functionDefinition = 'functionDefinition';
+
+
 
 export type FunctionSet = 
 | FuncDeclaration
 | FuncDeclarationPrime
-| CompoundStatement
-| Bi
+| compoundStatement
+| declarationPrime
+| FD1
+| functionDefinition
 ;
 
 
@@ -25,15 +31,33 @@ const FuncDeclaration: ProductionSet  = {
 const FuncDeclarationPrime: ProductionSet = {
   Semi: ['initializer', 'Semi'],
   Comma: ['Comma', 'FuncDeclaration'],
-  Equal: ['initializer', 'Semi'],
+  Assign: ['initializer', 'Semi'],
+}
 
+const declarationPrime: ProductionSet = {
+  Comma: ['Comma', 'declaration'],
+  RightParen: ['RightParen'],
 }
 
 const compoundStatement: ProductionSet = {
   LeftBrace: ['LeftBrace', 'bI', 'RightBrace']
 }
 
+const fD1: ProductionSet = {
+  Semi: ['Semi'],
+  LeftBrace: ['compoundStatement']
+}
+
 const bi: ProductionSet = {}
+
+const functionDefinition: ProductionSet = {
+  Const: ['declaration', 'fD1'],
+  Static: ['declaration', 'fD1'],
+  Char: ['declaration', 'fD1'],
+  Int: ['declaration', 'fD1'],
+  Bool: ['declaration', 'fD1'],
+  RightParen: ['RightParen', 'fD1'],
+}
 
 export const FunctionSyntaxProvider: ISyntaxProvider<FunctionSet> = {
   getSyntax(): ISyntax<FunctionSet> {
@@ -41,7 +65,9 @@ export const FunctionSyntaxProvider: ISyntaxProvider<FunctionSet> = {
       FuncDeclaration,
       FuncDeclarationPrime,
       compoundStatement,
-      bi,
+      declarationPrime,
+      fD1,
+      functionDefinition
     };
   },
 };
