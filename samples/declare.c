3d14233a9991742870480bf5674d5f69ebeaafac
David Massana <00072416@uca.edu.sv>
2022-11-30 23:43:05 -0600

    Adding typespecifiers for float and void

--- samples/declare.c
+++ samples/declare.c
@@ -1,15 +1,18 @@
 int x(int madres){
-return 0;
+
+  bool flag = true;
+
+  if (flag) {
+    printf("Bandera: true");
+  } else {
+    printf("Bandera: false");
+  }
+
+  return 0;
 }
 
 int main() {
   printf("Hello World");
 
   return 1545;
-}
-
-int main() {
-    for (int i=0; i>20; i){
-      printf(i);
-    }
 }
